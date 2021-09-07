655f8aeeba75ac4e9cc856c429a53ecba98e575b
Kim-jiyeon224 <shjh0107@gmail.com>
2021-09-07 18:11:57 +0900

    210907 jdbcTemplate, JPA, 스프링 데이터 JPA

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional //데이터를 저장하거나 변경할 때 있어야함
 public class MemberService {
     private final MemberRepository memberRepository;
 
